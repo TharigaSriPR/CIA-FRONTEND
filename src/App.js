7afca96ef259e7d5354eb83d23b67d7777feed3f
TharigaSriPR <100060856+TharigaSriPR@users.noreply.github.com>
2023-03-08 16:22:55 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Farmer from './Farmer';
+
+function App() {
+  return (
+    <div>
+        <Farmer/>
+    </div>
+    
+  );
+}
+
+export default App;
